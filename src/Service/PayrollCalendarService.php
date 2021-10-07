abe836d0e5ae239bad8e7239bdd87d560af40577
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-07 16:36:09 +0800

    Method to retieve and format the users timesheet data into a format we can use

--- src/Service/PayrollCalendarService.php
+++ src/Service/PayrollCalendarService.php
@@ -2,8 +2,11 @@
 
 namespace Dcodegroup\LaravelXeroTimesheetSync\Service;
 
+use App\Models\Timesheet;
+use App\Models\User;
 use Carbon\Carbon;
 use Dcodegroup\LaravelConfiguration\Models\Configuration;
+use Illuminate\Database\Eloquent\Builder;
 use XeroPHP\Models\PayrollAU\PayrollCalendar;
 
 class PayrollCalendarService
@@ -29,9 +32,7 @@ class PayrollCalendarService
             return [];
         }
 
-        $calendar = collect($this->configurationPayrollCalendars)->first(function ($value, $key) use ($payrollCalendarId) {
-            return data_get($value, 'PayrollCalendarID') == $payrollCalendarId;
-        });
+        $calendar = $this->getCalendar($payrollCalendarId);
 
         $calendarPeriodStarts = $this->buildCalendarPeriodStartDates($calendar);
 
@@ -48,6 +49,87 @@ class PayrollCalendarService
     public function getPayrollCalendarsFromConfiguration(): array
     {
         return $this->configurationPayrollCalendars;
+    }
+
+    public function getCalendarName(string $payrollCalendarId)
+    {
+        return $this->getName($this->getCalendar($payrollCalendarId));
+    }
+
+    public function retrieveUserTimeSheets(string $payrollCalendarPeriods = null, int $userId = null): array
+    {
+        if (is_null($payrollCalendarPeriods) || is_null($userId)) {
+            return [];
+        }
+
+        $user = User::find($userId);
+
+        //dd($payrollCalendarPeriods);
+        [
+            $tmpStartDate,
+            $tmpEndDate,
+        ] = explode('||', $payrollCalendarPeriods);
+
+        $startDate = Carbon::parse($tmpStartDate)->startOfDay();
+        $endDate = Carbon::parse($tmpEndDate)->endOfDay();
+
+        return Timesheet::query()
+            ->whereDate('start', '>=', $startDate)
+            ->whereDate('stop', '<=', $endDate)
+            ->whereHasMorph(
+                'timesheetable',
+                [User::class],
+                fn (Builder $builder) => $builder->where('id', $user->id)
+            )
+            ->get()
+            ->map(function ($timesheet) {
+                if ($timesheet->start->toDateString() != $timesheet->stop->toDateString()) {
+                    logger($timesheet->start->toDateTimeString());
+                    logger($timesheet->start->copy()->endOfDay());
+                    logger($timesheet->stop->copy()->startOfDay());
+                    logger($timesheet->stop->toDateTimeString());
+
+                    return [
+                        [
+                            // before midnight
+                            'date' => $timesheet->start->toDateString(),
+                            'units' => round($timesheet->start->floatDiffInHours($timesheet->start->copy()->endOfDay()->addSecond()), 2),
+                        ],
+                        [
+                            // after midnight
+                            'date' => $timesheet->stop->toDateString(),
+                            'units' => round($timesheet->stop->floatDiffInHours($timesheet->stop->copy()->startOfDay()), 2),
+                        ],
+                    ];
+                }
+
+                return [
+                    'date' => $timesheet->start->toDateString(),
+                    'units' => $timesheet->units,
+                ];
+            })
+            ->transform(function ($item) {
+                if (array_key_exists('date', $item)) {
+                    return [$item];
+                }
+
+                return $item;
+            })->flatten(1)
+            ->groupBy('date')
+            ->mapWithKeys(function ($item, $key) {
+                return [
+                    $key => [
+                        'units' => $item->sum('units'),
+                    ],
+                ];
+            })->toArray();
+    }
+
+    private function getCalendar(string $payrollCalendarId)
+    {
+        return collect($this->configurationPayrollCalendars)->first(function ($value, $key) use ($payrollCalendarId) {
+            return data_get($value, 'PayrollCalendarID') == $payrollCalendarId;
+        });
     }
 
     private function getMethodForCalendarType(array $calendar): string
@@ -75,6 +157,11 @@ class PayrollCalendarService
     private function getCalendarType(array $calendar): string
     {
         return data_get($calendar, 'CalendarType');
+    }
+
+    private function getName(array $calendar): string
+    {
+        return data_get($calendar, 'Name') ?? '';
     }
 
     private function getReferenceDate(array $calendar): Carbon
