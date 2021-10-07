9dd67613f1d49d79513d86b8e5735561311a25a9
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-07 16:34:45 +0800

    New controller preparing to send to xero

--- routes/laravel_xero_timesheet_sync.php
+++ routes/laravel_xero_timesheet_sync.php
@@ -1,8 +1,10 @@
 <?php
 
+use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\SendToXeroController;
 use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\XeroTimesheetPreviewController;
 use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\XeroTimesheetSummaryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('preview', XeroTimesheetPreviewController::class)->name('preview');
 Route::get('summary', XeroTimesheetSummaryController::class)->name('summary');
+Route::post('send-to-xero', SendToXeroController::class)->name('send-to-xero');
---
+++ src/Http/Controllers/SendToXeroController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Dcodegroup\LaravelXeroTimesheetSync\Service\PayrollCalendarService;
+use Illuminate\Http\Request;
+
+class SendToXeroController extends Controller
+{
+    protected PayrollCalendarService $service;
+
+    public function __construct(PayrollCalendarService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function __invoke(Request $request)
+    {
+
+        // dispatch send to xero
+
+        return response()->redirect('xero_timesheet_sync.preview ', ['user_id'                 => $request->input('user_id'),
+                                                                     'payroll_calendar'        => $request->input('payroll_calendar'),
+                                                                     'payroll_calendar_period' => $request->input('payroll_calendar_period')
+        ]);
+    }
+}
