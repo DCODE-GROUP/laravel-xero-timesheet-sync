394d8eef82d60998e56c35a949ea556c332eff97
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-12 15:41:02 +0800

    Add the middleware to prepopulate the values

--- routes/laravel_xero_timesheet_sync.php
+++ routes/laravel_xero_timesheet_sync.php
@@ -3,8 +3,9 @@
 use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\SendToXeroController;
 use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\XeroTimesheetPreviewController;
 use Dcodegroup\LaravelXeroTimesheetSync\Http\Controllers\XeroTimesheetSummaryController;
+use Dcodegroup\LaravelXeroTimesheetSync\Http\Middleware\LaravelXeroTimesheetSyncPreviewPrepopulate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('preview', XeroTimesheetPreviewController::class)->name('preview');
+Route::get('preview', XeroTimesheetPreviewController::class)->name('preview')->middleware(LaravelXeroTimesheetSyncPreviewPrepopulate::class);
 Route::get('summary', XeroTimesheetSummaryController::class)->name('summary');
 Route::post('send-to-xero/{xeroTimesheet}', SendToXeroController::class)->name('send-to-xero');
---
+++ src/Http/Middleware/LaravelXeroTimesheetSyncPreviewPrepopulate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dcodegroup\LaravelXeroTimesheetSync\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+
+class LaravelXeroTimesheetSyncPreviewPrepopulate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->filled('user_id')) {
+            $user = User::find($request->input('user_id'));
+
+            if (!$request->filled('payroll_calendar')) {
+                $request->request->set('payroll_calendar', $user->xero_default_payroll_calendar_id);
+            }
+        }
+
+        return $next($request);
+    }
+}
