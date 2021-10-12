8b5695525a180178b138ffc7952c368729f4f178
lionslair <lionslair@users.noreply.github.com>
2021-10-12 07:42:21 +0000

    Fix styling

--- src/Http/Middleware/LaravelXeroTimesheetSyncPreviewPrepopulate.php
+++ src/Http/Middleware/LaravelXeroTimesheetSyncPreviewPrepopulate.php
@@ -18,7 +18,7 @@ class LaravelXeroTimesheetSyncPreviewPrepopulate
         if ($request->filled('user_id')) {
             $user = User::find($request->input('user_id'));
 
-            if (!$request->filled('payroll_calendar')) {
+            if (! $request->filled('payroll_calendar')) {
                 $request->request->set('payroll_calendar', $user->xero_default_payroll_calendar_id);
             }
         }
